23a05dfb28db57b49dd6e122dbd7ada8e41f549d
khaoularj <khaoula.raja1@gmail.com>
2024-04-18 16:25:22 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,12 +5,12 @@
 def canUnlockAll(boxes):
     """this is a method that determines if all the boxes can be opened
     boxes is a list of lists"""
-    def dfs(box_index):
+    def explore(box_index):
         visited.add(box_index)
         for key in boxes[box_index]:
             if key not in visited and key < len(boxes):
-                dfs(key)
+                explore(key)
 
     visited = set()
-    dfs(0)
+    explore(0)
     return len(visited) == len(boxes)
